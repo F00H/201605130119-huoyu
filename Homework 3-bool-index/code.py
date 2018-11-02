d77f4c102518cbd3a2a484a723647f78550e5ddd
F0_0H <2219009383@qq.com>
2018-11-02 18:34:24 +0800

    Update code.py

--- Homework 3-bool-index/code.py
+++ Homework 3-bool-index/code.py
@@ -2,7 +2,7 @@ import json
 import nltk
 from nltk.corpus import stopwords
 
-def to_lowletter(word):
+def to_lowletter(word):           #大写转小写，以及去除非非拉丁字母
 	new_word = ''
 	for index in range(len(word)):
 		if word[index] >= 'A' and word[index] <= 'Z':
@@ -11,7 +11,7 @@ def to_lowletter(word):
 			new_word += word[index]
 	return new_word
 
-def tokenize(sentence):
+def tokenize(sentence):		#分词并去标点
 	sentence = nltk.word_tokenize(sentence)
 	#去掉标点符号
 	english_punctuations = [',', '.', ':', ';', '?', '(', ')', '[', ']', '&', '!', '*', '@', '#', '$', '%', '...']
@@ -28,7 +28,7 @@ word_list = []
 word_set = set()
 word_dic = {}
 article = []
-for i in range(len(data)):
+for i in range(len(data)): #分词并建表
 	sentence = tokenize(data[i]["text"])
 	if (len(sentence) > 2 and sentence[-2] == 'http'):
 		sentence = sentence[:-2]
@@ -37,14 +37,14 @@ for i in range(len(data)):
 	article.append(sentence)
 
 word_list = list(word_set)
-word_list.sort()
-for i in word_list:
+word_list.sort()	#排序
+for i in word_list:     #建立字典，即bool索引表
 	if i[0] in word_dic.keys():
 		word_dic[i[0]].append(i[1])
 	else:
 		word_dic[i[0]] = [i[1]]
 
-fp = open("bool.txt", "w", encoding = 'utf-8')
+fp = open("bool.txt", "w", encoding = 'utf-8') #把建好的表写入文件，以便以后方便使用
 for key, value in word_dic.items():
 	#value = [str(i) for i in value]
 	fp.write(key + " ")
@@ -54,11 +54,12 @@ for key, value in word_dic.items():
 fp.close()
 
 
+#开始测试
 
 import json
 import nltk
 
-def init():
+def init(): #从文件读入整个索引表
 	fp = open("bool.txt", "r")
 	word_dic = {}
 	for i in fp.readlines():
@@ -67,7 +68,7 @@ def init():
 		word_dic[j[0][:-1]] = [int(x) for x in k]
 	return word_dic
 
-def Or(x, y):
+def Or(x, y): #并操作
 	n = len(x)
 	m = len(y)
 	i = 0
@@ -86,7 +87,7 @@ def Or(x, y):
 			j += 1
 	return z
 
-def And(x, y):
+def And(x, y): #交操作
 	n = len(x)
 	m = len(y)
 	i = 0
@@ -103,7 +104,7 @@ def And(x, y):
 			j += 1
 	return z
 
-def Sub(x, y):
+def Sub(x, y): #减操作
 	y = And(x, y)
 	n = len(x)
 	m = len(y)
@@ -119,7 +120,7 @@ def Sub(x, y):
 			j += 1
 	return z		
 
-def Calc(s, word_dic):
+def Calc(s, word_dic):  #对于给定的操作序列，按顺序执行后返回操作结果
 	a = s.split()
 	n = len(a)
 	z = word_dic[a[0]]
@@ -137,10 +138,10 @@ def Calc(s, word_dic):
 
 def main():
 	word_dic = init()
-	s = 'shit & fuck'
+	s = 'shit & fuck' 
 	fp = open("article.txt", "r")
 	article = fp.readlines()
-	list0 = Calc(s, word_dic)
+	list0 = Calc(s, word_dic) #查找包含shit 和 fuck的所有文档
 	print(list0)
 	for i in list0:
 		print(article[i])
